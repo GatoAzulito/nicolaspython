02ea39d62890f9fea13973e503011ba8cdb05aef
gatoazulito <nicolas.wasaffv@alumnos.arcos.cl>
2026-04-17 11:28:18 -0400

    task task task

--- task.py
+++ task.py
@@ -1,8 +1,15 @@
 # for i in "Gatoazulito":
 #     print(i)
-counter=0
+c_vowels=0
+c_consonants=0
 name=str(input("Ingrese su nombre: "))
 for i in name:
-    print(i)
-    counter=counter+1
-print("La cantidad de letras es: ", counter)
+    if i.lower() in "aeiouáéíóúü":
+        print(i)
+        c_vowels=c_vowels+1
+    elif i==" ":
+        print("")
+    else:
+        c_consonants=c_consonants+1
+print("La cantidad de vocales es: ", c_vowels)
+print("La cantidad de consonantes es: ", c_consonants)
