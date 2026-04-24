e15200bb347c96612247dee65c163641b4c8dc1e
gatoazulito <nicolas.wasaffv@alumnos.arcos.cl>
2026-04-24 09:42:43 -0400

    switch y cases

---
+++ switchcase.py
@@ -0,0 +1,11 @@
+print("opcion 1")
+print("opcion 2")
+print("opcion 3")
+op=int(input("Elija una opción"))
+match op:
+    case 1:
+        print("Opcion 1")
+    case 2:
+        print("Opción 2")
+    case 3:
+        print("Opcion 3")
